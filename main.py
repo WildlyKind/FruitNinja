8627650fc2c60e43043b317ee68b4f684a4f4a51
natalia <python545420@gmail.com>
2025-01-28 20:13:46 +0300

    Фрукты разлетаются

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import pygame
 import os
 import sys
-from random import choice
 import random
+import time
 
 WHITE = pygame.Color("#ffffff")
 BLACK = pygame.Color("#000000")
@@ -31,6 +31,7 @@ def start_screen():
     screen.blit(fon, (0, 0))
     font = pygame.font.Font(None, 50)
     text_coord = 170
+    arr_rect = []
     for line in intro_text:
         string_rendered = font.render(line, 1, BLACK)
         intro_rect = string_rendered.get_rect()
@@ -39,22 +40,32 @@ def start_screen():
         intro_rect.x = 250
         text_coord += intro_rect.height
         screen.blit(string_rendered, intro_rect)
+        arr_rect.append(intro_rect)
+
+    arr_rect = arr_rect[1:]
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 terminate()
-            elif event.type == pygame.KEYDOWN or \
-                    event.type == pygame.MOUSEBUTTONDOWN:
+            elif event.type == pygame.KEYDOWN:
+                return
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                if arr_rect[0].collidepoint(event.pos):
+                    light()
+                elif arr_rect[1].collidepoint(event.pos):
+                    medium()
+                else:
+                    hard()
+
                 return  # начинаем игру
         pygame.display.flip()
         clock.tick(FPS)
 
+def count_result():
+    pass
 
 tile_width = tile_height = 50
-#images = [load_image('apple1.png'), load_image('lemon1.png'), load_image('pear1.png'),
-#          load_image('pineapple1.png'), load_image('watermelon1.png')]
-
 
 class HalfFruit(pygame.sprite.Sprite):
     def __init__(self, fruit, side, pos_x, pos_y):
@@ -65,27 +76,26 @@ class HalfFruit(pygame.sprite.Sprite):
 
     def update(self, *args):
         if self.side == 'left':
-            self.rect = self.rect.move(-5, 10)
+            self.rect = self.rect.move(-5, 8)
         else:
-            self.rect = self.rect.move(5, 10)
+            self.rect = self.rect.move(5, 8)
 
 
 class Fruits(pygame.sprite.Sprite):
-    def __init__(self, fruit_type):
+    def __init__(self, fruit_type, speed):
         super().__init__(fruits_group, all_sprites)
         self.fruit_type = fruit_type
         self.image = load_image(fruit_type + ".png")
         self.rect = self.image.get_rect()
-        self.speed = random.randint(1, 5)
+        self.speed = random.randint(1, speed)
         w, h = self.rect.size
         self.mask = pygame.mask.from_surface(self.image)
         self.rect = self.rect.move(random.randrange(0, WIDTH - w, 20), 0)
 
-
     def update(self, *args):
         self.rect = self.rect.move(0, self.speed)
         if self.rect.y > HEIGHT:
-            self.speed = random.randint(1, 6)
+            self.speed = random.randint(1, 5)
             self.rect.y = - self.rect.h
         if args:
             x, y = args[0]
@@ -93,31 +103,80 @@ class Fruits(pygame.sprite.Sprite):
                 self.rect.y = -50
                 HalfFruit(self.fruit_type + "_left.png", 'left', x, y)
                 HalfFruit(self.fruit_type + "_right.png", 'right', x, y)
-                #self.image = load_image('apple2.png')
+
+
+class Bomb(pygame.sprite.Sprite):
+    image = load_image("bomb.png")
+    image_boom = load_image("boom.png")
+
+    def __init__(self, speed):
+        super().__init__(bomb_group, all_sprites)
+        self.image = load_image('bomb.png')
+        self.rect = self.image.get_rect()
+        w, h = self.rect.size
+        self.rect = self.rect.move(random.randrange(0, WIDTH - w, 20), 0)
+        self.speed = random.randint(1, speed)
+
+    def update(self, *args, **kwargs):
+        self.rect = self.rect.move(0, self.speed)
+        if self.rect.y > HEIGHT:
+            self.speed = random.randint(1, self.speed)
+            self.rect.y = - self.rect.h
+        if args:
+            x, y = args[0]
+            #if flag: self.kill()
+            if self.rect.collidepoint(x, y):
+                self.image = Bomb.image_boom
+                self.speed = random.randint(3, 5)
+        if self.rect.y > 590:
+            self.image = Bomb.image
 
 
 fruits_group = pygame.sprite.Group()
 all_sprites = pygame.sprite.Group()
 half_fruits = pygame.sprite.Group()
+bomb_group = pygame.sprite.Group()
 
 d = ('lemon', 'apple', 'pear', 'pineapple', 'watermelon')
 
-for i in range(10):
-    fruit_type = random.choice(d)
-    Fruits(fruit_type)
+def light():
+    for i in range(10):
+        fruit_type = random.choice(d)
+        Fruits(fruit_type, 5)
+
+def medium():
+    for i in range(10):
+        fruit_type = random.choice(d)
+        Fruits(fruit_type, 5)
+    for i in range(5):
+        Bomb(5)
+
+def hard():
+    for i in range(12):
+        fruit_type = random.choice(d)
+        Fruits(fruit_type, 7)
+    for i in range(10):
+        Bomb(7)
 
 pygame.init()
+timer_interval = 1000 # 0.5 seconds
+timer_event = pygame.USEREVENT + 1
+pygame.time.set_timer(timer_event , timer_interval)
 screen = pygame.display.set_mode(SIZE)
 clock = pygame.time.Clock()
 pygame.display.set_caption('FRUIT NINJA')
 start_screen()
-#f = Fruits()
+result = 0
 while True:
     for event in pygame.event.get():
+        if event.type == timer_event:
+            print(3)
         if event.type == pygame.QUIT:
             terminate()
         if event.type == pygame.MOUSEBUTTONDOWN:
             all_sprites.update(event.pos)
+            result += 5
+            print(result)
     screen.fill(WHITE)
     all_sprites.draw(screen)
     all_sprites.update()
